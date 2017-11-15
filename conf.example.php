026b3f9c6a56bebc1b52aa95e3fcdc712f98461a
Leonel Faria <leonelf@gmail.com>
2017-11-15 10:49:30 +0000

    Renamed the example config file

---
+++ conf.example.php
@@ -0,0 +1,12 @@
+<?
+//Change to your bot token, check the readme to know how to create and get your bot token.
+define('BOT_TOKEN', 'DDDDDDDDD:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+
+//Set to TRUE if you want the bot only to awnser the trusted ChatIDs
+define('ONLYTRUSTED', TRUE);
+
+//Populate the array with the trusted Chat IDs
+$trusted = array(
+		'<chat_id>',
+	);
+
