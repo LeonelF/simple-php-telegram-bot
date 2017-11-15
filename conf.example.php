c3d021e0b6689ea07c22527dd138f0771d150e0c
Leonel Faria <leonelf@gmail.com>
2017-11-15 12:09:40 +0000

    Changed the array format #2

--- conf.example.php
+++ conf.example.php
@@ -1,10 +1,13 @@
 <?
-//Change to your bot token, check the readme to know how to create and get your bot token.
-$conf['bot_token'] = 'DDDDDDDDD:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
 
-//Set to TRUE if you want the bot only to awnser the trusted ChatIDs
-$conf['onlytrusted'] = TRUE;
-
-//Populate the array with the trusted Chat IDs
-$conf['trusted'] = [ '<chat_id>' ];
+$conf = [
+	//Change to your bot token, check the readme to know how to create and get your bot token.
+	'bot_token' => 'DDDDDDDDD:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+	//Set to TRUE if you want the bot only to awnser the trusted ChatIDs
+	'onlytrusted' => TRUE,
+	//Populate the array with the trusted Chat IDs
+	'trusted' => [ 
+		'<chat_id>' 
+		],
+	];
 
